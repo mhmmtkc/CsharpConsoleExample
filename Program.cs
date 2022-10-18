f4b29ac945049993280cfcdbc423258177ee3977
Muhammet Koc <muhammetkoc874@gmail.com>
2022-10-18 15:50:33 +0300

    added new variables

--- Program.cs
+++ Program.cs
@@ -13,7 +13,8 @@ namespace console_programlama
             string name = Console.ReadLine() ?? throw new Exception();
             Console.WriteLine("Soyisminizi girin");
             string surname = Console.ReadLine() ?? throw new Exception();
-            Console.WriteLine("Merhaba," + name + " " + surname);
+            string fullName = "Merhaba," + name + " " + surname;
+            Console.WriteLine(fullName);
             int deger = 5;
             float deger1 = 9;
             double d1 = 15.536;
@@ -23,7 +24,22 @@ namespace console_programlama
             Int64 i64 = 64;
             bool t1 = true;
             string tamIsim = name + " " + surname;
+            byte b = 5;   //1 byte
+            sbyte sb = 5; //1byte
+            short s2 = 5;   //2byte
+            ushort us = 5;  //2byte
+            uint ui = 8;      //4byte
+            ulong ul = 5;     //8byte
+            float f = 48;     //4byte
+            double d4 = 56.5;   //8bye
+            decimal de = 52;  //16byte
+            char c = 'd';         //2byte
+            object o1 = 'f';
+            object o2 = 5;
+            object o3 = "hi";
 
+            string age = "I am " + ui.ToString() + " years old.";
+            Console.WriteLine(age + Environment.NewLine + fullName + Environment.NewLine + us + Environment.NewLine + dt1);
 
 
         }
